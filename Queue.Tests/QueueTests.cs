b718a8344f61ff212b3ad544e430bbbe0551ec3d
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-07-25 19:07:32 +0300

    Extra tests were added.

--- Queue.Tests/QueueTests.cs
+++ Queue.Tests/QueueTests.cs
@@ -20,43 +20,189 @@
         {
             if (initialCapacity != null)
             {
-                return new EpamDay15.Queue<int>((int)initialCapacity).Capacity;
+                return new CustomQueue.Queue<int>((int)initialCapacity).Capacity;
             }
             
-            return new EpamDay15.Queue<int>().Capacity;
+            return new CustomQueue.Queue<int>().Capacity;
         }
 
         [TestCase(1, 5, 7, 8, 9, ExpectedResult = 8)]
         [TestCase(1, 5, 7, 8, 9, 1, 5, 7, 8, 9, ExpectedResult = 16)]
         [TestCase(new int[0], ExpectedResult = 4)]
         public int Constructor_ValidEnumerable_ValidCapacity(params int[] elements) =>
-            new EpamDay15.Queue<int>(elements.AsEnumerable()).Capacity;
+            new CustomQueue.Queue<int>(elements.AsEnumerable()).Capacity;
 
         [TestCase(3, 5, 7, 8, 9, ExpectedResult = 6)]
         [TestCase(5, 5, 7, 8, 9, 1, 5, 7, 8, 9, ExpectedResult = 10)]
         [TestCase(5, new int[0], ExpectedResult = 5)]
         public int Constructor_ValidEnumerableAndCapacity_ValidCapacity(int capacity, params int[] elements) =>
-            new EpamDay15.Queue<int>(elements.AsEnumerable(), capacity).Capacity;
+            new CustomQueue.Queue<int>(elements.AsEnumerable(), capacity).Capacity;
 
         [TestCase(1, 5, 7, 8, 9, ExpectedResult = 10)]
         [TestCase(1, 5, 7, 8, 9, 1, 5, 7, 8, 9, ExpectedResult = 20)]
         [TestCase(new int[0], ExpectedResult = 0)]
-        public int Constructor_ValidCollection_ValidCapacity(params int[] elements) => new EpamDay15.Queue<int>(elements).Capacity;
+        public int Constructor_ValidCollection_ValidCapacity(params int[] elements) => new CustomQueue.Queue<int>(elements).Capacity;
 
         [Test]
         public void Constructor_InvalidInitialCapacity_ThrowsArgumentOutOfRangeExc() =>
-            Assert.Throws<ArgumentOutOfRangeException>(() => new EpamDay15.Queue<int>(-1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new CustomQueue.Queue<int>(-1));
 
         [Test]
         public void Constructor_NullEnumerate_ThrowsArgumentNullExc() =>
-            Assert.Throws<ArgumentNullException>(() => new EpamDay15.Queue<int>((IEnumerable<int>)null));
+            Assert.Throws<ArgumentNullException>(() => new CustomQueue.Queue<int>((IEnumerable<int>)null));
 
         [Test]
         public void Constructor_ValidEnumerableInvalidInitialCapacity_ThrowsArgumentOutOfRangeExc() =>
-            Assert.Throws<ArgumentOutOfRangeException>(() => new EpamDay15.Queue<int>(new int[0].AsEnumerable(), -1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new CustomQueue.Queue<int>(new int[0].AsEnumerable(), -1));
 
         [Test]
         public void Constructor_NullCollection_ThrowsArgumentNullExc() =>
-            Assert.Throws<ArgumentNullException>(() => new EpamDay15.Queue<int>(null));
+            Assert.Throws<ArgumentNullException>(() => new CustomQueue.Queue<int>(null));
+
+        [Test]
+        public void Enumeration_ValidInput_ValidEnumerationOrder()
+        {
+            var elements = Enumerable.Range(1, 20).ToArray();
+            var q = new CustomQueue.Queue<int>(elements);
+
+            var zip = elements.Zip(q, (fromElements, fromQueue) => new { fromElements, fromQueue });
+            foreach (var value in zip)
+            {
+                if (value.fromElements != value.fromQueue)
+                {
+                    Assert.Fail("Not valid elements");
+                }
+            }
+        }
+
+        [Test]
+        public void Enqueue_ValidInput_ValidCapacityAndSizeAndEnumerationOrderAfterResizing()
+        {
+            var elements = Enumerable.Range(1, 20).ToArray();
+            var q = new CustomQueue.Queue<int>(elements.Take(5).ToArray());
+            foreach (int element in elements.Skip(5))
+            {
+                q.Enqueue(element);
+            }
+
+            var zip = elements.Zip(q, (fromElements, fromQueue) => new { fromElements, fromQueue });
+            foreach (var value in zip)
+            {
+                if (value.fromElements != value.fromQueue)
+                {
+                    Assert.Fail("Not valid elements");
+                }
+            }
+
+            if (q.Size != 20)
+            {
+                Assert.Fail($"Not valid size. Actual = {q.Size}");
+            }
+
+            if (q.Capacity != 20)
+            {
+                Assert.Fail($"Not valid capacity. Actual = {q.Capacity}");
+            }
+        }
+
+        [Test]
+        public void Dequeue_ValidInput_ValidCapacityAndSizeAndEnumerationOrderAfterResizing()
+        {
+            var elements = Enumerable.Range(1, 11).ToArray();
+            var q = new CustomQueue.Queue<int>();
+            foreach (int element in elements)
+            {
+                q.Enqueue(element);
+            }
+
+            q.Dequeue();
+            q.Dequeue();
+            q.Dequeue();
+
+            var zip = elements.Skip(3).Zip(q, (fromElements, fromQueue) => new { fromElements, fromQueue });
+            foreach (var value in zip)
+            {
+                if (value.fromElements != value.fromQueue)
+                {
+                    Assert.Fail("Not valid elements");
+                }
+            }
+
+            if (q.Size != 8)
+            {
+                Assert.Fail($"Not valid size. Actual = {q.Size}");
+            }
+
+            if (q.Capacity != 16)
+            {
+                Assert.Fail($"Not valid capacity. Actual = {q.Capacity}");
+            }
+        }
+
+        [Test]
+        public void Dequeue_ValidInput_ValidCapacityAndSizeAndEnumerationOrderAfterShrinking()
+        {
+            var elements = Enumerable.Range(1, 11).ToArray();
+            var q = new CustomQueue.Queue<int>();
+            foreach (int element in elements)
+            {
+                q.Enqueue(element);
+            }
+
+            q.Dequeue();
+            q.Dequeue();
+            q.Dequeue();
+
+            q.ShrinkToFit();
+
+            var zip = elements.Skip(3).Zip(q, (fromElements, fromQueue) => new { fromElements, fromQueue });
+            foreach (var value in zip)
+            {
+                if (value.fromElements != value.fromQueue)
+                {
+                    Assert.Fail("Not valid elements");
+                }
+            }
+
+            if (q.Size != 8)
+            {
+                Assert.Fail($"Not valid size. Actual = {q.Size}");
+            }
+
+            if (q.Capacity != 8)
+            {
+                Assert.Fail($"Not valid capacity. Actual = {q.Capacity}");
+            }
+        }
+
+        [Test]
+        public void Enumeration_EqueueInForeach_ThrowsInvalidOperationExc()
+        {
+            var q = new CustomQueue.Queue<int>(Enumerable.Range(1, 11));
+
+            Assert.Throws<InvalidOperationException>(
+                () =>
+                    {
+                        foreach (int _ in q)
+                        {
+                            q.Enqueue(42);
+                        }
+                    });
+        }
+
+        [Test]
+        public void Enumeration_DequeueInForeach_ThrowsInvalidOperationExc()
+        {
+            var q = new CustomQueue.Queue<int>(Enumerable.Range(1, 11));
+
+            Assert.Throws<InvalidOperationException>(
+                () =>
+                    {
+                        foreach (int _ in q)
+                        {
+                            q.Dequeue();
+                        }
+                    });
+        }
     }
 }
